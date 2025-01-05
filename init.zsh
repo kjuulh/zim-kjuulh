1bf7f10b6ec42b8361395304f2e14509486f5ba9
kjuulh <contact@kjuulh.io>
2025-01-05 17:58:44 +0100

    feat: back to ssh
    Signed-off-by: kjuulh <contact@kjuulh.io>

--- init.zsh
+++ init.zsh
@@ -190,7 +190,8 @@ dev() {
                           "$local_host" "exit" 2>/dev/null; then
         # If the test connection succeeded, make the actual connection
         echo "Connected locally"
-        MOSH_TITLE_NOPREFIX=1 mosh --no-init --ssh="ssh -t" "$local_host" -- "$zellij_cmd"
+        #MOSH_TITLE_NOPREFIX=1 mosh --no-init --ssh="ssh -t" "$local_host" -- "$zellij_cmd"
+        ssh -t "$local_host" "$zellij_cmd"
     else
         echo "Local connection failed, trying remote connection..."
         # Try remote connection
@@ -198,7 +199,8 @@ dev() {
                -o StrictHostKeyChecking=accept-new \
                "$remote_host" "exit" 2>/dev/null; then
             echo "Connected remotely"
-            MOSH_TITLE_NOPREFIX=1 mosh --no-init --ssh="ssh -t" "$remote_host" -- "$zellij_cmd"
+            #MOSH_TITLE_NOPREFIX=1 mosh --no-init --ssh="ssh -t" "$remote_host" -- "$zellij_cmd"
+            ssh -t "$remote_host" "$zellij_cmd"
         else
             echo "Error: Both local and remote connections failed"
             return 1
