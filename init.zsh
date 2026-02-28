6a12cff09d71874a4a37ac3134e4e2d475eb2cfe
kjuulh <contact@kjuulh.io>
2026-02-28 22:27:01 +0100

    fix: dev script

--- init.zsh
+++ init.zsh
@@ -302,15 +302,16 @@ preview_parquet() {
 }
 
 dev() {
+    setopt local_options no_monitor
+
     voidpin_port=$(shuf -i 30000-40000 -n 1)
     RUST_LOG=warn voidpin listen --grpc "0.0.0.0:$voidpin_port" >/dev/null 2>&1 &
     local pid=$!
-    disown $pid
 
     local tunnel_port=$(shuf -i 30000-40000 -n 1)
     local client_remote_ip="10.0.9.19"
     local server_remote_ip="10.0.9.18"
-    
+
     RUST_LOG=warn notunnel \
       --host "$client_remote_ip:$tunnel_port" \
       --server-host "$server_remote_ip" \
@@ -318,7 +319,6 @@ dev() {
       --client-host-port $tunnel_port \
       serve >/dev/null 2>&1 &
     local notunnel_pid=$!
-    disown $notunnel_pid 
      
     local timeout=5  # Timeout in seconds for local connection attempt
     local local_host="nef"
@@ -359,27 +359,31 @@ dev() {
     } always {
       {
           kill $pid 2>/dev/null
+          wait $pid 2>/dev/null
           kill $notunnel_pid 2>/dev/null
+          wait $notunnel_pid 2>/dev/null
       } 2>/dev/null
     }
 }
 
 dev_ratchet() {
+    setopt local_options no_monitor
+
     voidpin_port=$(shuf -i 30000-40000 -n 1)
-    RUST_LOG=warn voidpin listen --grpc "0.0.0.0:$voidpin_port" &
+    RUST_LOG=warn voidpin listen --grpc "0.0.0.0:$voidpin_port" >/dev/null 2>&1 &
     local pid=$!
 
     local tunnel_port=$(shuf -i 30000-40000 -n 1)
     local client_remote_ip="10.0.9.19"
     local server_remote_ip="10.0.9.1"
-    
+
     RUST_LOG=warn notunnel \
       --host "$client_remote_ip:$tunnel_port" \
       --server-host "$server_remote_ip" \
       --client-host "$client_remote_ip" \
       --client-host-port $tunnel_port \
-      serve &
-    local notunnel_pid=$! 
+      serve >/dev/null 2>&1 &
+    local notunnel_pid=$!
      
     local timeout=5  # Timeout in seconds for local connection attempt
     local local_host="ratchet"
@@ -418,8 +422,12 @@ dev_ratchet() {
           fi
       fi
     } always {
-      kill $pid 2>/dev/null
-      kill $tunnel_pid 2>/dev/null
+      {
+          kill $pid 2>/dev/null
+          wait $pid 2>/dev/null
+          kill $notunnel_pid 2>/dev/null
+          wait $notunnel_pid 2>/dev/null
+      } 2>/dev/null
     }
 }
 
